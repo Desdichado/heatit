458807b541f76dc0556ad525a85601916dd52b02
Pontus Kindblad <pontuskindblad@pontuss-air.lan>
2023-08-20 23:34:25 +0200

    trying with real format data

--- functions/sensorvalues.js
+++ functions/sensorvalues.js
@@ -1,21 +1,31 @@
-const { ElasticClient } = require('@elastic/elasticsearch')
-
+const { Client } = require('@elastic/elasticsearch')
+require('array.prototype.flatmap').shim();
 exports.handler = async (event, context) => {
-    const elasticClient = new ElasticClient({
-        node: 'https://9581bdf4cb5d4b4b9bc1482b84ee70d6.eu-central-1.aws.cloud.es.io',
+        const client = new Client({
+        cloud: {
+            id: 'kmit-production:ZXUtY2VudHJhbC0xLmF3cy5jbG91ZC5lcy5pbzo0NDMkOTU4MWJkZjRjYjVkNGI0YjliYzE0ODJiODRlZTcwZDYkOTkwYTNlMzk4YTk1NDc2NWIwNzhiNmJmZWViNTNlMGE=',
+        },
         auth: {
-            apiKey: { // API key ID and secret
-                id: 'elastic',
-                api_key: 'pvskHbs7Hg6FntlurwDwYi6o',
-              }
+                username: 'elastic',
+                password: 'pvskHbs7Hg6FntlurwDwYi6o',
         }
     });
-    const data = event.body;
-        // Validate the data structure
+    const data = JSON.parse(event.body);
+    /*    // Validate the data structure
         if (!Array.isArray(data)) {
-            return res.status(400).json({ message: "Data should be an array of sensor readings" });
+            return {
+                statusCode: 400,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': '*',
+                    'Access-Control-Allow-Headers': '*',
+                    'Access-Control-Allow-Credentials': 'true'
+                },
+                body: "{message: 'Data should be an array of sensor readings', data: " + JSON.stringify(data) + "}"
+            };
         }
-    
+    */
         const bulkData = [];
         for (const entry of data) {
             bulkData.push(JSON.stringify({
@@ -26,12 +36,12 @@ exports.handler = async (event, context) => {
             }));
         }
         // Save data locally
-        fs.appendFileSync(DATA_FILE, bulkData.join('\n'));
+        //fs.appendFileSync(DATA_FILE, bulkData.join('\n'));
     
         // Forward data to Elasticsearch
         const operations = bulkData.flatMap(doc => [{ index: { _index: 'sandbatteri-stream-dev' } }, doc])
 
-        const bulkResponse = await elasticClient.bulk({ refresh: true, operations })
+        const bulkResponse = await client.bulk({ refresh: true, operations })
       
         if (bulkResponse.errors) {
           const erroredDocuments = []
@@ -55,8 +65,19 @@ exports.handler = async (event, context) => {
           console.log(erroredDocuments)
         }
       
-        const count = await elasticClient.count({ index: 'tweets' })
+        const count = await client.count({ index: 'sandbatteri-stream-dev' })
         console.log(count)
+        return {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Credentials': 'true'
+            },
+            body: "{count: " + count.count + "}"
+        };
       }
     
     return {
@@ -68,5 +89,5 @@ exports.handler = async (event, context) => {
             'Access-Control-Allow-Headers': '*',
             'Access-Control-Allow-Credentials': 'true'
         },
-        body: count
+        body: "{status: 'ok'}"
     };
