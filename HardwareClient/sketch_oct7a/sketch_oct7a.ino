9ebe8cd2f76d8d8ef9d4af30c565279c753598bf
Christoffer Möckelind <christoffer@bambuser.com>
2024-01-07 01:43:38 +0100

    arduino: Refactor arduino code for better readability
    Quite a massive change.
    
    Some things we do:
    
    - Use class to contain temperature sensors. Seems like we are going to
      have a few different kinds so some light abstraction is nice.
    - Add enum for sensor locations. This makes sense here because it's at
      the generating end. We ofc. expect this to change as time goes so we
      shouldn't expect this to be a stable interface. In the future, we
      could add some validation that there is only one sensor per location
      and that all locations are covered.
    - Remove sensor id. I'm open to discuss ofc, but I though that it would
      be easier to just have one distinct location per distinct sensor. We
      can always add the id back if we need it later.
    - Use http lib to talk to sensordata api.
    - Use a separate .h file for the wifi credentials. This is not ideal
      but it's better than having them in the main file.
    - Use json lib for serializing the data to send to the api instead
      of doing it manually. This will make things much more flexible in
      the future when we want to add more data to the payload.

--- HardwareClient/sketch_oct7a/sketch_oct7a.ino
+++ HardwareClient/sketch_oct7a/sketch_oct7a.ino
@@ -1,3 +1,4 @@
+#include <ArduinoHttpClient.h>
 #include <LiquidCrystal.h>
 #include <ArduinoJson.h>
 #include "WiFiS3.h"
@@ -7,60 +8,103 @@
 #include <DallasTemperature.h>
 #include "max6675.h"
 
+#include "wifiConfig.h"
 
-
-#define PARAMETERS JsonObject
-//Tempsensor
-// Data wire is plugged into pin 2 on the Arduino , You are free to take another one
+// Tempsensor
+//  Data wire is plugged into pin 2 on the Arduino , You are free to take another one
 #define ONE_WIRE_BUS 13
 
-DynamicJsonDocument doc(2048);
-
-char ssid[] = "kmesh";     //wifi network name
-char pass[] = "1Ske.aip";  //wifi network password
-//char ssid[] = "B-WiFi";     //wifi network name
-//char pass[] = "K8nd2bSX9?";  //wifi network password
-
 int status = WL_IDLE_STATUS;
-const char server[] = "heatit.k-mit.se";  // name address for server using dns
-const int port = 443;
-unsigned long lastConnectionTime = 0;               // last time you connected to the server, in milliseconds
-const unsigned long postingInterval = 10L * 1000L;  // delay between updates, in milliseconds
+
+// Api path constants
+const char apiHost[] = "heatit.k-mit.se"; // name address for server using dns
+const int apiPort = 443;
+const char apiPath[] = "/.netlify/functions/sensorvalues";
+
+unsigned long lastConnectionTime = 0;              // last time you connected to the server, in milliseconds
+const unsigned long postingInterval = 10L * 1000L; // delay between updates, in milliseconds
 int thermoDO = 6;
 int thermoCS = 7;
 int thermoCLK = 8;
 int sensorCount = 2;
-char sensorpathstring[100] = "/.netlify/functions/sensorvalues";
 
-String nextSend = "";
-
-//HARDWARE INIT
+// HARDWARE INIT
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-int heaterPins[] = { 9, 10, 13 };
+int heaterPins[] = {9, 10, 13};
 int fanPin = 0;
 int pumpPin = 1;
-WiFiSSLClient client;
+WiFiSSLClient wifiClient;
+HttpClient httpClient = HttpClient(wifiClient, apiHost, apiPort);
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
-OneWire oneWire(ONE_WIRE_BUS);        // Setup a oneWire instance to communicate with any OneWire devices
-DallasTemperature sensors(&oneWire);  // Pass our oneWire reference to Dallas Temperature.
-//testvärden
+OneWire oneWire(ONE_WIRE_BUS);       // Setup a oneWire instance to communicate with any OneWire devices
+DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature.
+// testvärden
 
-typedef struct {
-  String type;
-  String id;
-} SensorStruct;
-SensorStruct sensorTries[2] = {
-  (SensorStruct){ "temp water", "waterTank" },
-  (SensorStruct){ "temp pipe", "heaterExit" }
+enum ThermometerLocation
+{
+  waterTank,
+  heaterExit
 };
 
+char *sensorLocationToString(ThermometerLocation location)
+{
+  switch (location)
+  {
+  case waterTank:
+    return "waterTank";
+  case heaterExit:
+    return "heaterExit";
+  default:
+    return "unknown";
+  }
+}
 
+class Thermometer
+{
+public:
+  Thermometer(ThermometerLocation location)
+  {
+    this->location = location;
+  }
+  void writeSensorReading(JsonObject &json)
+  {
+    json["location"] = sensorLocationToString(this->getLocation());
+    json["temperatureC"] = this->getTemperatureC();
+  }
+  virtual int getTemperatureC()
+  {
+    return 0;
+  }
+  ThermometerLocation getLocation()
+  {
+    return this->location;
+  }
 
-//**********************************************************************************
-void setup() {
-  //serializeJson(createJson, createJsonString);
+private:
+  ThermometerLocation location;
+};
 
-  //Initialize serial lcd and sensor and wait for serial port to open:
+class MockThermometer : public Thermometer
+{
+public:
+  MockThermometer(ThermometerLocation location) : Thermometer(location) {}
+  int getTemperatureC() override
+  {
+    return random(30, 90);
+  }
+};
+
+// // Default values, should be over written if running in the wild
+Thermometer *thermometers[] = {
+    new MockThermometer(ThermometerLocation::waterTank),
+    new MockThermometer(ThermometerLocation::heaterExit),
+};
+
+void setup()
+{
+  // serializeJson(createJson, createJsonString);
+
+  // Initialize serial lcd and sensor and wait for serial port to open:
   Serial.begin(9600);
   lcd.begin(16, 2);
   sensors.begin();
@@ -74,173 +118,132 @@ void setup() {
 
 //**********************************************************************************
 
-
-void loop() {
-  //sensors.requestTemperatures();  // Send the command to get temperatures from dallas sensors.
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
+void loop()
+{
+  // sensors.requestTemperatures();  // Send the command to get temperatures from dallas sensors.
+  //  if the server's disconnected, stop the client:
+  if (!wifiClient.connected())
+  {
     lcd.clear();
     lcd.print("disconnecting from server.");
-    client.stop();
+    wifiClient.stop();
+  }
+
+  JsonDocument requestBody;
+  JsonArray sensorReadings = requestBody.to<JsonArray>();
+
+  // Iterate through all the thermometers and add their readings to the requestBody
+  for (auto &thermometer : thermometers)
+  {
+    JsonObject sensorReading = sensorReadings.add<JsonObject>();
+    thermometer->writeSensorReading(sensorReading);
   }
   // Print the temps of the dallas sensors to the lcd and add it to be sent to server*********
-  String sensorstring;
+  String requestBodyString;
 
-  nextSend = "[";
-  for (int a = 0; a < sensorCount; a++) {
-    float tempsensorstring = random(30, 90);  //sensors.getTempCByIndex(a);
-    int tempsensorcount = a;
-    addSensorValueToNextSend(tempsensorstring, sensorTries[a].type, sensorTries[a].id);
-    sensorstring = tempsensorstring + String(tempsensorcount) + String(":") + tempsensorstring + String(" ");
+  // Use pretty json for easier debugging
+  serializeJsonPretty(requestBody, requestBodyString);
+  Serial.println("requestBodyString: ");
+  Serial.println(requestBodyString);
+  httpClient.post(apiPath, "application/json", requestBodyString);
+  int responseStatus = httpClient.responseStatusCode();
+  String responseBodyString = httpClient.responseBody();
+  JsonDocument responseBody;
+  DeserializationError deserializationError = deserializeJson(responseBody, responseBodyString);
+  if (deserializationError)
+  {
+    Serial.println("Failed to deserialize response body from sensorvalues api. Error: " + String(deserializationError.f_str()));
+    Serial.println("responseBody: \n" + responseBodyString);
   }
-  nextSend = nextSend.substring(0, nextSend.lastIndexOf(","));
-  nextSend += "]";
-  lcd.clear();
-  //  Serial.println(sensorstring);
-  httpRequest();
-  delay(8000);
-  read_response();
-  // *********
-  delay(20000);
-}
-
-// { "@timestamp": "2099-05-06T16:21:15.000Z", "sensorValue": 26.1, "sensorType": "temp", "sensorId": "d2" }
-void addSensorValueToNextSend(float sensorValue, String sensorType, String sensorId) {
-  nextSend += "{\"sensorType\": \"" + sensorType + "\", \"sensorId\": \"" + sensorId + "\", \"sensorValue\": " + sensorValue + "},";
-}
-
-// this method makes a HTTP connection to the server:
-/* -------------------------------------------------------------------------- */
-void httpRequest() {
-  /* -------------------------------------------------------------------------- */
-  if (nextSend.length() < 1) {
-    Serial.println("Nothing to send. Returning.");
-    return;
+  if (responseStatus != 200 || responseStatus != 201 || responseStatus != 202)
+  {
+    Serial.println("Failed request to report new sensor values and get new commands from sensorvalues api. Status code is not 200, 201 or 202, was: " + String(responseStatus));
+    Serial.println("responseBody: \n" + responseBodyString);
   }
-  client.stop();
-  Serial.println("Connectiong to " + String(server) + " on port: " + String(port));
-  Serial.println("with size: " + String(nextSend.length() + 1));
-  client.setTimeout(1000);
-  // if theres a successful connection:
-  if (client.connect(server, port)) {
-    Serial.println("connecting to: " + String(server) + String(sensorpathstring));
 
-    // send the HTTP POST request:
-    client.println("POST " + String(sensorpathstring) + " HTTP/1.1");
-    client.println("Host: " + String(server));
-    client.println("Content-Type: application/json");
-    client.println("Content-Length: " + String(nextSend.length() + 1));
-    client.println("User-Agent: ArduinoWiFi/1.1");
-    client.println("Connection: close");
-    client.println();
-    client.println(nextSend + String("\n"));
-    Serial.println(nextSend + String("\n"));
-  } else {
-    // if you couldn't make a connection:
-    Serial.println("connection failed");
-  }
-}
-
-/* just wrap the received data up to 80 columns in the serial print*/
-/* -------------------------------------------------------------------------- */
-void read_response() {
-  /* -------------------------------------------------------------------------- */
-  // Check HTTP status
-
-  String response = "";
-  unsigned long timeout = 10000;  // 10 seconds timeout
-  unsigned long startTime = millis();
-
-  while (client.connected() || client.available()) {
-    while (client.available()) {
-      char c = client.read();
-      response += c;
-    }
-
-    if (millis() - startTime > timeout) {
-      Serial.println(F("Timeout while reading response."));
-      break;
-    }
-  }
-  int startstr = response.indexOf("{ \"message");
-  int stopstr = response.indexOf("}]}");
-  String responseCut = response.substring(startstr, stopstr + 3);
-  Serial.println(responseCut);  // Print the full response
-
-  DeserializationError error = deserializeJson(doc, responseCut);
-  if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
-    return;
-  }
-  const char* command = doc["command"];
-  Serial.println(command);
-
-  JsonArray commandsArray = doc["commands"];
-  for (JsonObject commandObj : commandsArray) {
-    const char* cmd = commandObj["command"];
-    const char* value = commandObj["value"];
+  JsonArray commands = responseBody["commands"];
+  for (JsonObject command : commands)
+  {
+    const char *cmd = command["command"];
+    const char *value = command["value"];
     Serial.print("Command: ");
     Serial.print(cmd);
     Serial.print(", Value: ");
     Serial.println(value);
     controlRelay(cmd, value);
   }
-  // Disconnect
-  client.stop();
+
+  delay(30000);
 }
 
-void controlRelay(String device, String value) {
+void controlRelay(String device, String value)
+{
   int pinStatus = (value == "on") ? HIGH : LOW;
-  if (device == "heater") {
+  if (device == "heater")
+  {
     int elementCount = sizeof(heaterPins) / sizeof(int);
-    for (int a = 0; a < elementCount; a++) {
+    for (int a = 0; a < elementCount; a++)
+    {
       digitalWrite(heaterPins[a], pinStatus);
       Serial.println("Turned heater on for pin " + String(heaterPins[a]) + " " + value);
     }
-  } else {
+  }
+  else
+  {
     int pin = (device == "fan") ? fanPin : pumpPin;
     digitalWrite(pin, pinStatus);
     Serial.println("Turned " + device + " on. pin " + String(pin));
   }
 }
 
-void connectWiFi() {
+void connectWiFi()
+{
   // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
+  if (WiFi.status() == WL_NO_MODULE)
+  {
     lcd.clear();
-    lcd.print("Communication with WiFi module failed!");
+    String errorMessage = "Communication with WiFi module failed!";
+    lcd.print(errorMessage);
+    Serial.println(errorMessage);
     // don't continue
     while (true)
       ;
   }
 
   String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+  {
     lcd.clear();
-    lcd.print("Please upgrade the firmware");
+    String errorMessage = "Please upgrade the firmware";
+    lcd.print(errorMessage);
+    Serial.println(errorMessage);
   }
 
   // attempt to connect to WiFi network:
   int counter = 1;
-  while (status != WL_CONNECTED) {
+  while (status != WL_CONNECTED)
+  {
     lcd.clear();
-    lcd.print("try ");
-    lcd.print(counter);
-    lcd.print(" SSID: ");
+    lcd.print("try " + String(counter) + " SSID: ");
     lcd.setCursor(0, 1);
-    lcd.print(ssid);
+    lcd.print(wifi_ssid);
+    Serial.println("Attempting to connect to SSID: " + String(wifi_ssid) + "\nAttempt: " + String(counter) + "...");
     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
-
+    status = WiFi.begin(wifi_ssid, wifi_password);
     // wait 7 seconds for connection:
     delay(7000);
+    if (status != WL_CONNECTED)
+    {
+      Serial.println("Connecting to " + String(wifi_ssid) + " failed. Will keep retrying until connection is successful ...");
+    }
     counter++;
   }
+  Serial.println("Successfully connected to wifi " + String(wifi_ssid));
 }
 
 /* -------------------------------------------------------------------------- */
-void printWifiStatus() {
+void printWifiStatus()
+{
   /* -------------------------------------------------------------------------- */
   lcd.clear();
   // print the SSID of the network you're attached to:
@@ -251,25 +254,22 @@ void printWifiStatus() {
   IPAddress ip = WiFi.localIP();
   lcd.print("IP: ");
   lcd.println(ip);
-
-  // print the received signal strength:
-  /*long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");*/
 }
 
-void waitForSerialtoStart() {
-  while (!Serial) {
+void waitForSerialtoStart()
+{
+  while (!Serial)
+  {
     lcd.clear();
-    lcd.print("waiting for serial port to connect");  //Needed for native USB port only
+    lcd.print("waiting for serial port to connect"); // Needed for native USB port only
   }
-  return;
+  Serial.println("Serial port connected");
 }
 
-void setSensorCount() {
-  if (sensors.getDeviceCount() > 0) {
+void setSensorCount()
+{
+  if (sensors.getDeviceCount() > 0)
+  {
     sensorCount = sensors.getDeviceCount();
   }
-  return;
 }
